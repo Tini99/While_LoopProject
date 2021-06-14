0537e98e93913d9f3e97a5087660da1abfaf90ca
Tini Gisa <1814759@nzse.ac.nz>
2021-06-14 18:24:28 +1200

    Add project files.

---
+++ While_LoopProject/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace While_LoopProject
+{
+    /*
+     Program to print odd numbers between 1 to 20 using while loop
+    "Number1" is used as a integer
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number1;
+
+            Number1 = 1;
+            While(Number1 <= 20);
+            {
+                Console.WriteLine(Number1);
+                Number1 = Number1 + 2;
+            }
+
+        }
+    }
+}
